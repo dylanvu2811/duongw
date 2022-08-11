126fe18d04e89741f9f0f0c32d54a04406466e08
duongvu <duongseo2811@gmail.com>
2022-08-11 14:39:52 +0700

    tsst

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 123
+          Learn React okie đc r
         </a>
       </header>
     </div>
